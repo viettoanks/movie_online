7e2fc04cd37f46bc60dc23d392dfba783b57086d
Ly Van Minh <lyvanminh1002@gmail.com>
2018-10-09 22:11:54 +0700

    Static_pages

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,8 @@ module MovieOnline
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}")]
+    config.i18n.available_locales = [:en, :vi]
+    config.i18n.default_locale = :en
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,5 @@
 Rails.application.routes.draw do
+  root "static_pages#home"
+  get "/home", to: "static_pages#home", as: :home
+  get "/help", to: "static_pages#help", as: :help
 end
